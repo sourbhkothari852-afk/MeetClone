f71e0b983f75a653930bb8e034316012a0c805a3
sourbh <sourbhkothari852@gmail.com>
2026-09-02 10:47:27 +0530

    Configure Django settings for Vercel

--- MeetClone/settings.py
+++ MeetClone/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-_w25bj6b(3o_dn-1*6j^qo05j6uk!sacn7v$+o-4dw=!qm6q2l'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app',]
 
 
 # =========================================================
