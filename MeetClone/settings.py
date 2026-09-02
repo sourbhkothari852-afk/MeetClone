8c1aa07ee9fd6ca72af26e52378ca0dd3b9d25f6
sourbh <sourbhkothari852@gmail.com>
2026-09-02 17:34:31 +0530

    Fix database URL configuration

--- MeetClone/settings.py
+++ MeetClone/settings.py
@@ -116,27 +116,27 @@ WSGI_APPLICATION = "MeetClone.wsgi.application"
 
 ASGI_APPLICATION = "MeetClone.asgi.application"
 
-
 # =========================================================
 # DATABASE
 # =========================================================
-#
-# Local computer:
-#   SQLite database
-#
-# Vercel production:
-#   PostgreSQL DATABASE_URL
-#
-# =========================================================
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # =========================================================
 # PASSWORD VALIDATION
